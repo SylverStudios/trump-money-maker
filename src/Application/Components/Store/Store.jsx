53277cd44101ac92b40d174de6a3e1ac48274a58
Aaron Votre <avotre422@gmail.com>
2016-05-15 11:23:55 -0400

    Use _.partial

--- src/Application/Components/Store/Store.jsx
+++ src/Application/Components/Store/Store.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StoreItem from './StoreItem';
+import _ from 'underscore';
 
 const Store = React.createClass({
   propTypes: {
@@ -12,17 +13,14 @@ const Store = React.createClass({
         <div id="store-zone">
           <h3>Broker</h3>
           <div id="item-menu">
-            {this.props.assets.map((asset, index) => {
-              const onClick = () => {
-                this.props.onItemClick(index);
-              };
+            {this.props.assets.map((asset) => {
               return (
                 <StoreItem
                   key={asset.name}
                   name={asset.name}
                   price={asset.price}
                   owned={asset.owned}
-                  onClick={onClick}
+                  onClick={_.partial(this.props.onItemClick, asset.id)}
                 />
               );
             })}
