62a1569cdf0a9589d9751d64f20d4f02f9c75608
Aaron Votre <avotre422@gmail.com>
2016-05-17 19:18:02 -0400

    Enable keybound components and asset constants
    Added a new library for keybindings and added the constants file.
    Currently, these are not wired up to anything.

---
+++ src/Application/Components/Shop/KeyBoundShopItem.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Mousetrap from 'mousetrap';
+import { assetDefaults } from './../../../util/constants';
+
+
+class KeyBoundShopItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.keyCode = assetDefaults[this.props.name].keyCode
+  }
+
+  componentDidMount() {
+    Mousetrap.bind([this.keyCode], this.props.onClick);
+  }
+
+  componentWillUnmount() {
+    Mousetrap.unbind(this.keyCode);
+  }
+
+  render() {
+    const imgSrc = `images/${this.props.name}.png`;
+
+    return (
+      <div className="shop-item panel panel-default" onClick={this.props.onClick}>
+        <div className="panel-body">
+          <img className="col-md-2" src={imgSrc}/>
+          <div className="shop-item-content col-md-8">
+            <div className="shop-item-title">{this.props.name}</div>
+            <div className="shop-item-cost">${this.props.price.toFixed(2)}</div>
+          </div>
+          <div className="shop-item-number col-md-2">{this.props.owned}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+KeyBoundShopItem.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  onClick: React.PropTypes.func.isRequired,
+  owned: React.PropTypes.number.isRequired,
+  price: React.PropTypes.number.isRequired,
+};
+
+export default KeyBoundShopItem;
---
+++ src/util/constants.js
@@ -0,0 +1,59 @@
+// KeyCodes
+const ONE = 48;
+const TWO = 49;
+const THREE = 50;
+const FOUR = 51;
+const FIVE = 52;
+const SIX = 53;
+const SEVEN = 54;
+const EIGHT = 55;
+const NINE = 56;
+export const KEYS = [ ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE ];
+
+export const assetDefaults = {
+  'Tenement': {
+    keyCode: '1',
+    baseIncome: 0.1,
+    basePrice: 50,
+  },
+  'Hotel': {
+    keyCode: '2',
+    baseIncome: 1,
+    basePrice: 150,
+  },
+  'Golf Course': {
+    keyCode: '3',
+    baseIncome: 9,
+    basePrice: 750,
+  },
+  'Casino': {
+    keyCode: '4',
+    baseIncome: 200,
+    basePrice: 2000,
+  },
+  'Trump Tower': {
+    keyCode: '5',
+    baseIncome: 800,
+    basePrice: 5000,
+  },
+  'Trump Town': {
+    keyCode: '6',
+    baseIncome: 2000,
+    basePrice: 20000,
+  },
+  'Trump City': {
+    keyCode: '7',
+    baseIncome: 10000,
+    basePrice: 100000,
+  },
+  'Governership': {
+    keyCode: '8',
+    baseIncome: 200000,
+    basePrice: 400000,
+  },
+  'Trump ISS': {
+    keyCode: '9',
+    baseIncome: 999999,
+    basePrice: 9999999,
+  }
+};
