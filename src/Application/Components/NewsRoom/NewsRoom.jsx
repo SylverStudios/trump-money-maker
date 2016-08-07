071718765d6686dfedea1e56073dc82732bfb0c6
Aaron Votre <avotre422@gmail.com>
2016-08-07 16:02:46 -0400

    Add broadcast classes for styling

--- src/Application/Components/NewsRoom/NewsRoom.jsx
+++ src/Application/Components/NewsRoom/NewsRoom.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Headline from './Headline';
+import Broadcast from '../../Models/Broadcast';
 
 const NewsRoom = React.createClass({
   propTypes: {
@@ -13,14 +14,34 @@ const NewsRoom = React.createClass({
       <div className="news-queue">
         <ul>
           <Headline content={this.props.broadcasts[0].content}/>
-          {oldNews.map((broadcast, index) => {
-            return (
-              <li className="news-text" key={index}><hr/>{broadcast.content}</li>
-            );
-          })}
+          {oldNews.map(this.renderBroadcast)}
         </ul>
       </div>
     );
+  },
+
+  renderBroadcast(broadcast, index) {
+    switch (broadcast.type) {
+      case Broadcast.TYPES.INSTRUCTION:
+        return (
+          <li className="news-text broadcast-instruction" key={index}><hr/>{broadcast.content}</li>
+        );
+
+      case Broadcast.TYPES.FLAVOR:
+        return (
+          <li className="news-text broadcast-flavor" key={index}><hr/>{broadcast.content}</li>
+        );
+
+      case Broadcast.TYPES.NEWS:
+        return (
+          <li className="news-text broadcast-news" key={index}><hr/>{'Latest: ' + broadcast.content}</li>
+        );
+
+      default:
+        return (
+          <li className="news-text" key={index}><hr/>{broadcast.content}</li>
+        );
+    }
   },
 
   render() {
