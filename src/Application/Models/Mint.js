51eb21ec7d82814bcb7569bdc0a2b2eae454c3b3
Aaron Votre <avotre422@gmail.com>
2016-11-02 19:11:38 -0400

    Nerf denominations

--- src/Application/Models/Mint.js
+++ src/Application/Models/Mint.js
@@ -1,16 +1,16 @@
 import Denomination from './Denomination';
 
 const denominations = [
-  new Denomination('penny', 0.01, undefined),
-  new Denomination('nickel', 0.05, 1),
-  new Denomination('dime', 0.10, 2),
-  new Denomination('quarter', 0.25, 5),
-  new Denomination('dollar', 1, 20),
-  new Denomination('fiveDollar', 5, 100),
-  new Denomination('tenDollar', 10, 200),
-  new Denomination('twentyDollar', 20, 400),
-  new Denomination('fiftyDollar', 50, 1000),
-  new Denomination('hundredDollar', 100, 2000),
+  new Denomination('penny', 0.01, undefined), //
+  new Denomination('nickel', 0.05, 0.5),         // 10%
+  new Denomination('dime', 0.10, 1.5),          // 6.7%
+  new Denomination('quarter', 0.25, 5),         // 5%
+  new Denomination('dollar', 1, 30),            // 3.33%
+  new Denomination('fiveDollar', 5, 200),       // 2.5%
+  new Denomination('tenDollar', 10, 400),       // 2.5%
+  new Denomination('twentyDollar', 20, 800),    // 2.5%
+  new Denomination('fiftyDollar', 50, 1500),    // 3.3%
+  new Denomination('hundredDollar', 100, 2000), // 5%
 ];
 
 class Mint {
